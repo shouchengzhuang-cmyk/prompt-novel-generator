014a88a7c3def3de00a7d5456f979ce09cc8dc70
shouchengzhuang-cmyk <shouchengzhuang@gmail.com>
2026-05-19 12:42:38 +0800

    fix: avoid duplicate dev server startup

--- start-dev.ps1
+++ start-dev.ps1
@@ -1,5 +1,3 @@
-﻿Set-Location "D:\Projects\prompt-novel-generator"
-
-Start-Process powershell -ArgumentList '-NoExit', '-Command', 'cd "D:\Projects\prompt-novel-generator\server"; node index.js'
+Set-Location "D:\Projects\prompt-novel-generator"
 
 Start-Process powershell -ArgumentList '-NoExit', '-Command', 'cd "D:\Projects\prompt-novel-generator"; npm run dev'
